3b2554ede37c7efebeaae9d8c73bfc3e56fc1684
TuckoxXL <sebastianbelluccini@gmail.com>
2023-04-17 15:55:41 -0300

    Initial commit

---
+++ Assets/Scrip/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float Forcemultiplier;
+    public float jumforce;
+    public bool canjump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        canjump = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalforce = Input.GetAxis("Horizontal") * Forcemultiplier;
+        //float verticalforce = Input.GetAxis("Vertical") * Forcemultiplier;
+
+        horizontalforce *= Time.deltaTime;
+        //verticalforce *= Time.deltaTime;
+        transform.Translate(horizontalforce, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && canjump)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, jumforce, 0);
+            canjump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            canjump = true;
+        }
+    }
+}
