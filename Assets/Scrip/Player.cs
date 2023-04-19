6f337a2a37768b7f5b4a710f652242a44d765a50
TuckoxXL <sebastianbelluccini@gmail.com>
2023-04-19 17:19:24 -0300

    1.2

--- Assets/Scrip/Player.cs
+++ Assets/Scrip/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -39,5 +40,11 @@ public class Player : MonoBehaviour
         {
             canjump = true;
         }
+
+        if (collision.gameObject.CompareTag("Pinchos"))
+        {
+            SceneManager.LoadScene("");
+        }
     }
+
 }
